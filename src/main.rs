7c93d2a398b7958d30c53c529f0534e0dc1bbffa
Nova-archx86 <moorcode66@yahoo.com>
2021-11-17 15:01:07 -0800

    cleaned up main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,47 +8,32 @@ fn main() {
     println!("1. add\n2. sub\n3. mult\n4. div\n");
 
     let choice = utils::read_int();
+    let mut cal = Calculator::new(); 
+    println!("Enter ther first number: "); 
+    cal.num1 = utils::read_float();
+    println!("Enter the second number: ");
+    cal.num2 = utils::read_float();
+    
     match choice {
         1 => {
-            let mut cal = Calculator::new();
-            println!("Enter a number: ");
-            cal.num1 = utils::read_float();
-            println!("Enter a number: ");
-            cal.num2 = utils::read_float();
             let res = cal.add();
             println!("Result: {}", res);
         }
 
         2 => {
-            let mut cal = Calculator::new();
-            println!("Enter a number: ");
-            cal.num1 = utils::read_float();
-            println!("Enter a number: ");
-            cal.num2 = utils::read_float();
-            let res = cal.sub();
+            let res = cal.sub();  
             println!("Result: {}", res);
         }
 
         3 => {
-            let mut cal = Calculator::new();
-            println!("Enter a number: ");
-            cal.num1 = utils::read_float();
-            println!("Enter a number: ");
-            cal.num2 = utils::read_float();
             let res = cal.mult();
             println!("Result: {}", res);
         }
 
         4 => {
-            let mut cal = Calculator::new();
-            println!("Enter a number: ");
-            cal.num1 = utils::read_float();
-            println!("Enter a number: ");
-            cal.num2 = utils::read_float();
             let res = cal.div();
             println!("Result: {}", res);
         }
-
         _ => {
             println!("Not a valid operation");
         }
